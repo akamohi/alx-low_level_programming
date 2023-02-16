bc89d4a5c5c46f8f189acf8afe1cbae60314b721
sobek <myusername@gmail.com>
2023-02-16 11:39:00 -0800

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,14 +1,11 @@
 #include <stdio.h>
-
 /**
- * main - Entry point
- *
- * Return: Always 0 (success) 
+ * Description: main - Prints out a string to stdout.
+ * Return: 0 if success.
  */
 int main(void)
 {
-puts("\"programming is like building a multilingual puzzle");
-
+puts("\"Programming is like building a multilingual puzzle");
 return (0);
 }
 
