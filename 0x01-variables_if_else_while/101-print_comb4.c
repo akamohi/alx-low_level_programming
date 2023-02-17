51d9c8d2839f760491c841059f137834b7fe90d5
sobek <myusername@gmail.com>
2023-02-17 15:20:41 -0800

    101-print_comb4.c

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -1,42 +1,35 @@
 #include <stdio.h>
 
 /**
- * main - program that prints all possible different combinations of
- * three digits
- * Numbers must be separated by ,followed by a space
- * The two digits must be different
- * 012, 120, 102, 021, 201, 210 are considered the same combination
- * Return: 0
+ * main - Prints all possible combinations of three different digits,
+ *        in ascending order, separated by a comma followed by a space.
+ *
+ * Return: Always 0.
  */
-
 int main(void)
 {
-	int n1 = 48;
-	int n2, n3;
-	int com = 44;
+	int digit1, digit2, digit3;
 
-	while (n1 <= 57)
+	for (digit1 = 0; digit1 < 8; digit1++)
 	{
-		n2 = n1 + 1;
-		while (n2 <= 57)
+		for (digit2 = digit1 + 1; digit2 < 9; digit2++)
 		{
-			n3 = n2 + 1;
-			while (n3 <= 57)
+			for (digit3 = digit2 + 1; digit3 < 10; digit3++)
 			{
-				putchar(n1);
-				putchar(n2);
-				putchar(n3);
-				if (n1 != 55 || n2 != 56 || n3 != 57)
-				{
-					putchar(com);
-					putchar(32);
-				}
-				n3 += 1;
+				putchar((digit1 % 10) + '0');
+				putchar((digit2 % 10) + '0');
+				putchar((digit3 % 10) + '0');
+
+				if (digit1 == 7 && digit2 == 8 && digit3 == 9)
+					continue;
+				
+				putchar(',');
+				putchar(' ');
 			}
-			n2 += 1;
 		}
-		n1 += 1;
 	}
+
 	putchar('\n');
+
 	return (0);
 }
