56a410d15b1a70a331cc1925adbe6973ccdc3dbd
sobek <myusername@gmail.com>
2023-02-21 05:46:59 -0800

    0. _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,17 @@
+#include "stdio.h"
+
+/*
+ * write the program that prints _putchar
+ *
+ */
+int main (void)
+{
+	_putchar('s');
+        _putchar('o');
+	_putchar('b');
+	_putchar('e');
+	_putchar('k');
+        _putchar('\n');
+
+	return (0);
+}
