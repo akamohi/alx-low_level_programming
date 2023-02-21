97f2187df45c38cf540e1d203b146c8f40bca2de
sobek <myusername@gmail.com>
2023-02-21 07:49:41 -0800

    1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,16 +1,18 @@
-#include "main.h"
+#include "holberton.h"
 
 /**
- * print_alphabet - make the alphhabet
- *
- * Return: void 
- */
+  * print_alphabet - Make the alphabet
+  *
+  * Return: void
+  */
 void print_alphabet(void)
 {
-char c;
-for (c = 'a'; c <= 'z'; c++)
-{
-	_putchar(c);
-}
-_putchar('\n');
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+	{
+		_putchar(c);
+	}
+
+	_putchar('\n');
 }
